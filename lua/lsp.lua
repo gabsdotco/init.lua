8d7f43ec416ad610d84511256e1a8d416710b9de
gabsdotco <biewxw@gmail.com>
2023-07-18 15:10:22 -0300

    Revert "fix: fixed some lsp-zero breaking changes"
    This reverts commit 4e3ec7367cef35fc7b03e975d5769dcec73af6c7.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,23 +1,24 @@
--- LSP Zero Setup
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero')
 
+lsp.preset('recommended')
 lsp.ensure_installed({
   'tsserver',
   'eslint',
   'lua_ls',
 })
 
-lsp.set_sign_icons({
-  error = '•',
-  warn = '•',
-  hint = '•',
-  info = '•'
+lsp.set_preferences({
+  sign_icons = {
+    error = '•',
+    warn = '•',
+    hint = '•',
+    info = '•'
+  }
 })
 
 lsp.nvim_workspace()
 lsp.setup()
 
--- Neovim LSP Diagnostics
 vim.diagnostic.config({
   virtual_text = true,
 })
@@ -33,17 +34,3 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     update_in_insert = true,
   }
 )
-
--- LSP CMP Setup
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-
-cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }
-})
