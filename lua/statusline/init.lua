f56c6056cda7cdf9168c7dee20cc5d8cfd51c63c
Gabriel de Oliveira <biewxw@gmail.com>
2023-10-15 23:36:26 -0300

    feat(statusline): recovered the file component

--- lua/statusline/init.lua
+++ lua/statusline/init.lua
@@ -2,6 +2,7 @@ require("statusline.components.mode")
 require("statusline.components.branch")
 require("statusline.components.diagnostics")
 require("statusline.components.saved")
+require("statusline.components.file")
 
 require("statusline.utils")
 require("statusline.colors")
@@ -9,6 +10,7 @@ require("statusline.colors")
 function status_line()
   local mode = get_mode_component()
   local branch = get_branch_component()
+  local file = get_file_component()
   local errors = get_errors_component()
   local warnings = get_warnings_component()
   local infos = get_infos_component()
@@ -17,6 +19,7 @@ function status_line()
   return table.concat {
     get_component("StatusMode", mode),
     get_component("StatusBranch", branch),
+    get_component("StatusFile", file),
     get_component_separator(),
     get_component("StatusSaved", saved),
     get_component("StatusErrors", errors),
