73ff4da51ecbbba7aeb4c40845dd02140e87eab3
Gabriel de Oliveira <biewxw@gmail.com>
2023-10-15 23:35:30 -0300

    fix(telescope): removed buffers remap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -59,7 +59,6 @@ telescope.load_extension('file_browser')
 
 -- Telescope Remaps
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<C-t>', builtin.buffers, {})
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
 vim.keymap.set('n', '<C-s>', builtin.git_status, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
