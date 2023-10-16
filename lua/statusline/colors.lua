c115f8748afaa8bded5149b06cb282e8f07963dc
Gabriel de Oliveira <biewxw@gmail.com>
2023-10-15 21:39:34 -0300

    feat(statusline): new info component color + defaul color for branch statuses

--- lua/statusline/colors.lua
+++ lua/statusline/colors.lua
@@ -4,7 +4,10 @@ end
 
 highlight("StatusMode", "#000000", "#ffffff")
 highlight("StatusBranch", "#ffffff", "NONE")
-highlight("StatusFile", "#444444", "NONE")
+highlight("StatusFile", "#333333", "NONE")
 highlight("StatusSaved", "#adb5bd", "NONE")
 highlight("StatusWarnings", "#F6C177", "NONE")
 highlight("StatusErrors", "#EB6F92", "NONE")
+highlight("StatusInfos", "#89B4FA", "NONE")
+
+highlight("BranchComponentStatus", "#444444", "NONE")
