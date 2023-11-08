2b3eca6937a613006372ea6e347c15e9c58611d4
gabsdotco <biewxw@gmail.com>
2023-11-08 18:40:01 -0300

    feat(colors): moved from monochrome to semi macaroni colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,9 +7,13 @@ require("noirbuddy").setup({
     undercurl = true,
   },
   colors = {
-    background = "#0a0a0a",
-    primary = '#333333',
-    secondary = '#666666',
+    background = "#101010",
+    -- primary = '#333333',
+    -- secondary = '#666666',
+    -- primary = '#fefa67',
+    primary = '#fefa67',
+    secondary = '#a978f6',
+    tertiary = '#f2ff8a',
     noir_0 = '#f1f1f1',
     noir_1 = '#e2e2e2',
     noir_2 = '#c4c4c4',
@@ -26,6 +30,27 @@ require("noirbuddy").setup({
 -- Tokens colors
 vim.cmd("highlight @comment guifg=#383838")
 
+-- NeoTree Git colors
+vim.cmd("highlight NeoTreeGitDirty guifg=#ff8989")
+vim.cmd("highlight NeoTreeGitNew guifg=#f2ff8a")
+vim.cmd("highlight NeoTreeGitStaged guifg=#89B4FA")
+vim.cmd("highlight NeoTreeGitUnstaged guifg=#addef8")
+vim.cmd("highlight NeoTreeGitUntracked guifg=#F0C674")
+vim.cmd("highlight NeoTreeGitRenamed guifg=#f2ff8a")
+vim.cmd("highlight NeoTreeGitIgnored guifg=#333333")
+vim.cmd("highlight NeoTreeGitMerge guifg=#89B4FA")
+vim.cmd("highlight NeoTreeGitModified guifg=#addef8")
+vim.cmd("highlight NeoTreeGitDeleted guifg=#ff8989")
+vim.cmd("highlight NeoTreeMessage guifg=#333333")
+
+vim.cmd("highlight NeoTreeFileName guifg=#555555")
+vim.cmd("highlight NeoTreeFolderName guifg=#666666")
+
+vim.cmd("highlight NeoTreeFloat guibg=NONE")
+vim.cmd("highlight NeoTreeFloatTitle guibg=#101010 guifg=#e2e2e2")
+vim.cmd("highlight NeoTreeFloatBorder guibg=NONE guifg=#111111")
+vim.cmd("highlight NeoTreeFloatNormal guibg=#111111 guifg=#e2e2e2")
+
 -- Editor transparent colors
 vim.cmd("highlight Normal guibg=none")
 vim.cmd("highlight NonText guibg=none")
@@ -36,7 +61,7 @@ vim.cmd("highlight clear LineNr")
 vim.cmd("highlight clear SignColumn")
 
 -- LSP colors
-vim.cmd("highlight FloatBorder guibg=NONE guifg=#111111")
+vim.cmd("highlight FloatBorder guibg=NONE guifg=#222222")
 vim.cmd("highlight FloatShadow guibg=NONE guifg=NONE")
 vim.cmd("highlight FloatShadowThrough guibg=NONE guifg=NONE")
 
@@ -57,21 +82,21 @@ vim.cmd("highlight TabLineFill guibg=NONE guifg=NONE")
 vim.cmd("highlight StatusLine guibg=#0a0a0a")
 
 -- Split colors
-vim.cmd("highlight VertSplit guibg=NONE guifg=#111111")
+vim.cmd("highlight VertSplit guibg=NONE guifg=#101010")
 
 -- Command line colors
-vim.cmd("highlight Pmenu guibg=#0d0d0d guifg=#444444")
+vim.cmd("highlight Pmenu guibg=#111111 guifg=#444444")
 vim.cmd("highlight PmenuSel guibg=#181818 guifg=#ffffff")
-vim.cmd("highlight PmenuSbar guibg=#0a0a0a guifg=#0f0f0f")
+vim.cmd("highlight PmenuSbar guibg=#101010 guifg=#0f0f0f")
 vim.cmd("highlight PmenuThumb guibg=#181818 guifg=#0f0f0f")
 
 -- Fold colors
 vim.cmd("highlight Folded guibg=NONE guifg=#3b3b3b")
 
 -- Gitsigns colors
-vim.cmd("highlight GitSignsAdd guifg=#8AE234")
-vim.cmd("highlight GitSignsChange guifg=#89B4FA")
-vim.cmd("highlight GitSignsDelete guifg=#EB6F92")
+vim.cmd("highlight GitSignsAdd guifg=#f2ff8a")
+vim.cmd("highlight GitSignsChange guifg=#addef8")
+vim.cmd("highlight GitSignsDelete guifg=#ff8989")
 
 -- Telescope colors
 vim.cmd("highlight TelescopeBorder guibg=#0c0c0c guifg=#0c0c0c")
