c6d4da5ac84b7c20b513aaa344faaf0f6c7f6109
gabsdotco <biewxw@gmail.com>
2023-11-08 18:40:19 -0300

    feat(diagnostics): moved diagnostics configs to its own file

---
+++ after/plugin/diagnostics.lua
@@ -0,0 +1,14 @@
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    underline = true,
+    virtual_text = {
+      spacing = 5,
+      severity_limit = 'Warning',
+    },
+    update_in_insert = true,
+  }
+)
+
+-- LSP Diagnostic Remaps
+vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, {}) -- Open line diagnostics popup
