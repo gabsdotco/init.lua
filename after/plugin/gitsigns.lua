78379ac5a59ff0f4d1a7750e48a5aba60eb3018f
gabsdotco <biewxw@gmail.com>
2024-01-16 13:21:49 -0300

    fix(gitsigns): removed current line git blame to avoid lagging

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,5 +1,5 @@
 require('gitsigns').setup {
-  current_line_blame = true,
+  current_line_blame = false, -- will change it to a better plugin to avoid the lag
   signs = {
     add          = { hl = 'GitSignsAdd', text = '┃', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
     change       = { hl = 'GitSignsChange', text = '┃', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
