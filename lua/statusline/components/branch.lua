66aabb1be71cb0fce97e0a2573f7542dbcc3204f
gabsdotco <biewxw@gmail.com>
2024-01-22 08:35:55 -0300

    fix(statusline): fixed branch render issues

--- lua/statusline/components/branch.lua
+++ lua/statusline/components/branch.lua
@@ -1,54 +1,17 @@
-local function get_total_files_created()
-  local total_files_created = vim.fn.system("git ls-files --others --exclude-standard | wc -l")
-
-  if string.find(total_files_created, "fatal:") then
-    return ""
-  end
-
-  total_files_created = total_files_created:gsub("%s+", "")
-
-  if total_files_created == "0" then
-    return ""
-  end
-
-  return " %#BranchComponentStatus#(+" .. total_files_created .. ")"
-end
-
-local function get_total_files_changed()
-  local total_files_changed = vim.fn.system("git diff --name-only | wc -l")
-
-  if string.find(total_files_changed, "fatal:") then
-    return ""
-  end
-
-  total_files_changed = total_files_changed:gsub("%s+", "")
-
-  if total_files_changed == "0" then
-    return ""
-  end
-
-  return " %#BranchComponentStatus#(~" .. total_files_changed .. ")"
-end
-
 function get_branch_component()
-  local branch = vim.fn.system("git branch --show-current")
-
-  if string.find(branch, "fatal:") then
+  if not vim.g.loaded_fugitive then
     return ""
   end
 
-  branch = branch:gsub("%s+", "")
+  local branch = vim.fn.FugitiveHead(10)
 
-  if branch == "" then
-    return ""
+  if branch ~= "" then
+    if string.len(branch) > 15 then
+      branch = branch:sub(1, 15) .. "..."
+    end
+
+    return " " .. branch
   end
 
-  if string.len(branch) > 15 then
-    branch = branch:sub(1, 15) .. "..."
-  end
-
-  local created = get_total_files_created()
-  local changed = get_total_files_changed()
-
-  return " " .. branch .. created .. changed
+  return ""
 end
