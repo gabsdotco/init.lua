013d637287aee8065bf318ef332dd12548a0628c
gabsdotco <biewxw@gmail.com>
2024-05-27 10:04:04 -0300

    feat(null-ls): add eslint_d code actions

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,6 +4,7 @@ local sources = {
 	null_ls.builtins.formatting.prettier,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.diagnostics.eslint_d,
+	null_ls.builtins.code_actions.eslint_d,
 }
 
 local group = vim.api.nvim_create_augroup("null_ls_format_on_save", { clear = false })
--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,82 +1,82 @@
 local function my_on_attach(bufnr)
-	local api = require("nvim-tree.api")
+  local api = require("nvim-tree.api")
 
-	local function opts(desc)
-		return {
-			desc = "nvim-tree: " .. desc,
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			nowait = true,
-		}
-	end
+  local function opts(desc)
+    return {
+      desc = "nvim-tree: " .. desc,
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true,
+    }
+  end
 
-	-- default mappings
-	api.config.mappings.default_on_attach(bufnr)
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
 
-	-- custom mappings
-	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-	vim.keymap.set("n", "<C-s>", api.node.open.vertical, opts("Open: Vertical Split"))
+  -- custom mappings
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+  vim.keymap.set("n", "<C-s>", api.node.open.vertical, opts("Open: Vertical Split"))
 
-	-- remove default mappings
-	vim.keymap.del("n", "L", { buffer = bufnr })
-	vim.keymap.del("n", "<C-t>", { buffer = bufnr })
+  -- remove default mappings
+  vim.keymap.del("n", "L", { buffer = bufnr })
+  vim.keymap.del("n", "<C-t>", { buffer = bufnr })
 end
 
 require("nvim-tree").setup({
-	on_attach = my_on_attach,
-	sort = {
-		sorter = "case_sensitive",
-	},
-	view = {
-		width = 38,
-	},
-	update_focused_file = {
-		enable = true,
-	},
-	renderer = {
-		group_empty = false,
-		root_folder_label = false,
-		highlight_git = true,
-		special_files = {},
-		icons = {
-			git_placement = "after",
-			modified_placement = "after",
-			diagnostics_placement = "after",
-			show = {
-				folder_arrow = false,
-			},
-			web_devicons = {
-				file = {
-					enable = true,
-					color = false,
-				},
-				folder = {
-					enable = false,
-					color = false,
-				},
-			},
-			glyphs = {
-				default = "",
-				symlink = "",
-				git = {
-					unstaged = "",
-					staged = "",
-					unmerged = "",
-					renamed = "󰁕",
-					untracked = "",
-					deleted = "",
-					ignored = "",
-				},
-			},
-		},
-		indent_markers = {
-			enable = true,
-			inline_arrows = false,
-		},
-	},
-	filters = {
-		git_ignored = false,
-		dotfiles = true,
-	},
+  on_attach = my_on_attach,
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 38,
+  },
+  update_focused_file = {
+    enable = true,
+  },
+  renderer = {
+    group_empty = false,
+    root_folder_label = false,
+    highlight_git = true,
+    special_files = {},
+    icons = {
+      git_placement = "after",
+      modified_placement = "after",
+      diagnostics_placement = "after",
+      show = {
+        folder_arrow = false,
+      },
+      web_devicons = {
+        file = {
+          enable = true,
+          color = false,
+        },
+        folder = {
+          enable = false,
+          color = false,
+        },
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "",
+          unmerged = "",
+          renamed = "󰁕",
+          untracked = "",
+          deleted = "",
+          ignored = "",
+        },
+      },
+    },
+    indent_markers = {
+      enable = true,
+      inline_arrows = false,
+    },
+  },
+  filters = {
+    git_ignored = false,
+    dotfiles = true,
+  },
 })
