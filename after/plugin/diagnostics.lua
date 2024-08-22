810c93579645320e305ce1d4f5ac3cead95884ae
gabsdotco <biewxw@gmail.com>
2024-08-22 02:37:14 -0300

    fix(diagnostics): fix diagnostics api for new neovim version

--- after/plugin/diagnostics.lua
+++ after/plugin/diagnostics.lua
@@ -1,10 +1,10 @@
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
 	underline = true,
+	update_in_insert = true,
 	virtual_text = {
 		spacing = 4,
-		severity_limit = "Warning",
+		min = "Warning",
 	},
-	update_in_insert = true,
 })
 
 -- LSP Diagnostic Config
