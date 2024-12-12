424a170e3b6fef48caf534826b4b3ac5246a52c8
gabsdotco <biewxw@gmail.com>
2024-12-12 01:19:08 -0300

    feat(statusline): hide diagnostics when empty

--- lua/custom/statusline/components/diagnostics.lua
+++ lua/custom/statusline/components/diagnostics.lua
@@ -1,14 +1,29 @@
 function get_warnings_component()
 	local warning_count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
-	return warning_count
+
+	if warning_count > 0 then
+		return warning_count
+	end
+
+	return ""
 end
 
 function get_errors_component()
 	local error_count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
-	return error_count
+
+	if error_count > 0 then
+		return error_count
+	end
+
+	return ""
 end
 
 function get_infos_component()
 	local info_count = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
-	return info_count
+
+	if info_count > 0 then
+		return info_count
+	end
+
+	return ""
 end
