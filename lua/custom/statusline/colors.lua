8ce52c19a24206a0c35aeee7cb1e970802792005
gabsdotco <biewxw@gmail.com>
2024-12-16 19:08:36 -0300

    fix(colors): add small adjustment to the statusline colors

--- lua/custom/statusline/colors.lua
+++ lua/custom/statusline/colors.lua
@@ -2,7 +2,7 @@ local function highlight(tag, foreground, background, style)
 	vim.cmd("highlight " .. tag .. " guifg=" .. foreground .. " guibg=" .. background .. " gui=" .. style)
 end
 
-highlight("StatusMode", "#000000", Palette.text, "bold")
+highlight("StatusMode", Palette.noir_9, Palette.text, "bold")
 highlight("StatusBranch", Palette.text, "NONE", "NONE")
 highlight("StatusBranchChanges", Palette.noir_5, "NONE", "NONE")
 highlight("StatusFile", Palette.noir_5, "NONE", "NONE")
@@ -10,5 +10,3 @@ highlight("StatusSaved", Palette.noir_2, "NONE", "NONE")
 highlight("StatusWarnings", Palette.yellow, "NONE", "NONE")
 highlight("StatusErrors", Palette.red, "NONE", "NONE")
 highlight("StatusInfos", Palette.blue, "NONE", "NONE")
-
-highlight("BranchComponentStatus", "#444444", "NONE", "NONE")
