86d8c0823b44223871fbb48f0d4fd641032a8983
gabsdotco <biewxw@gmail.com>
2025-04-08 19:32:44 -0300

    feat(plugins): add copilot plugin

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+	"zbirenbaum/copilot.lua",
+	opts = {
+		suggestion = {
+			enabled = true,
+			debounce = 75,
+			auto_trigger = true,
+			hide_during_completion = true,
+			trigger_on_accept = true,
+			keymap = {
+				accept = "<C-j>",
+			},
+		},
+	},
+}
