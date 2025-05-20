b83099f1fe1bd78796bd3669998fb2eb76f928be
gabsdotco <biewxw@gmail.com>
2025-05-20 00:28:40 -0300

    refactor(*): fix files format

--- lua/plugins/none.lua
+++ lua/plugins/none.lua
@@ -1,33 +1,33 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  event = "VeryLazy",
-  opts = function()
-    local group = vim.api.nvim_create_augroup("LspFormatting", {})
-    local nls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	event = "VeryLazy",
+	opts = function()
+		local group = vim.api.nvim_create_augroup("LspFormatting", {})
+		local nls = require("null-ls")
 
-    return {
-      sources = {
-        nls.builtins.formatting.stylua,
-        nls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = group,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                bufnr = bufnr,
-              })
-            end,
-          })
-        end
-      end,
-    }
-  end,
+		return {
+			sources = {
+				nls.builtins.formatting.stylua,
+				nls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = group,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								bufnr = bufnr,
+							})
+						end,
+					})
+				end
+			end,
+		}
+	end,
 }
--- lua/plugins/spectre.lua
+++ lua/plugins/spectre.lua
@@ -1,36 +1,36 @@
 return {
-  "nvim-pack/nvim-spectre",
-  opts = {
-    color_devicons = true,
-    open_cmd = "vnew",
-    live_update = false,
-    lnum_for_results = true,
-    line_sep_start = "",
-    result_padding = "¦  ",
-    line_sep = "",
-    highlight = {
-      ui = "String",
-      search = "DiffChange",
-      replace = "DiffDelete",
-    },
-  },
-  config = function()
-    local spectre = require("spectre")
+	"nvim-pack/nvim-spectre",
+	opts = {
+		color_devicons = true,
+		open_cmd = "vnew",
+		live_update = false,
+		lnum_for_results = true,
+		line_sep_start = "",
+		result_padding = "¦  ",
+		line_sep = "",
+		highlight = {
+			ui = "String",
+			search = "DiffChange",
+			replace = "DiffDelete",
+		},
+	},
+	config = function()
+		local spectre = require("spectre")
 
-    vim.keymap.set("n", "<leader>S", function()
-      spectre.toggle()
-    end, { desc = "Toggle Spectre" })
+		vim.keymap.set("n", "<leader>S", function()
+			spectre.toggle()
+		end, { desc = "Toggle Spectre" })
 
-    vim.keymap.set("n", "<leader>sw", function()
-      spectre.open_visual({ select_word = true })
-    end, { desc = "Search current word" })
+		vim.keymap.set("n", "<leader>sw", function()
+			spectre.open_visual({ select_word = true })
+		end, { desc = "Search current word" })
 
-    vim.keymap.set("v", "<leader>sw", function()
-      spectre.open_visual()
-    end, { desc = "Search current word" })
+		vim.keymap.set("v", "<leader>sw", function()
+			spectre.open_visual()
+		end, { desc = "Search current word" })
 
-    vim.keymap.set("n", "<leader>sp", function()
-      spectre.open_file_search({ select_word = true })
-    end, { desc = "Search on current file" })
-  end,
+		vim.keymap.set("n", "<leader>sp", function()
+			spectre.open_file_search({ select_word = true })
+		end, { desc = "Search on current file" })
+	end,
 }
