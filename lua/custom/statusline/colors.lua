891e5d8868b07e483f35f898d4a2379e620ab086
gabsdotco <biewxw@gmail.com>
2025-11-05 15:03:25 -0300

    refactor(theme): add new light palette + initial toggle theme command

--- lua/custom/statusline/colors.lua
+++ lua/custom/statusline/colors.lua
@@ -1,12 +1,19 @@
+require("config.colors.dark")
+require("config.colors.light")
+
 local function highlight(tag, foreground, background, style)
 	vim.cmd("highlight " .. tag .. " guifg=" .. foreground .. " guibg=" .. background .. " gui=" .. style)
 end
 
-highlight("StatusMode", Palette.text, Palette.none, Palette.none)
-highlight("StatusBranch", Palette.text, Palette.none, Palette.none)
-highlight("StatusBranchChanges", Palette.noir_5, Palette.none, Palette.none)
-highlight("StatusFile", Palette.noir_4, Palette.none, Palette.none)
-highlight("StatusSaved", Palette.noir_2, Palette.none, Palette.none)
-highlight("StatusWarnings", Palette.yellow, Palette.none, Palette.none)
-highlight("StatusErrors", Palette.red, Palette.none, Palette.none)
-highlight("StatusInfos", Palette.blue, Palette.none, Palette.none)
+local is_light_mode = vim.o.background == "light"
+
+local colors = is_light_mode and PaletteLight or PaletteDark
+
+highlight("StatusMode", is_light_mode and colors.noir_0 or colors.text, colors.none, colors.none)
+highlight("StatusBranch", is_light_mode and colors.noir_1 or colors.text, colors.none, colors.none)
+highlight("StatusBranchChanges", is_light_mode and colors.noir_4 or colors.noir_5, colors.none, colors.none)
+highlight("StatusFile", is_light_mode and colors.noir_3 or colors.noir_4, colors.none, colors.none)
+highlight("StatusSaved", is_light_mode and colors.noir_5 or colors.noir_2, colors.none, colors.none)
+highlight("StatusWarnings", colors.yellow, colors.none, colors.none)
+highlight("StatusErrors", colors.red, colors.none, colors.none)
+highlight("StatusInfos", colors.blue, colors.none, colors.none)
