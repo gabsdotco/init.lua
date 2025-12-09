75c5d055b8ef8422816a6b8b70bd4fe04a245073
gabsdotco <biewxw@gmail.com>
2025-12-09 01:11:49 -0300

    refactor(bufferline): move bufferline highlights to colorscheme groups

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,3 @@
-require("config.colors.dark")
-require("config.colors.light")
-
 return {
 	"akinsho/bufferline.nvim",
 	lazy = false,
@@ -25,100 +22,12 @@ return {
 		},
 	},
 	config = function()
-		local is_light_mode = vim.o.background == "light"
-		local colors = is_light_mode and PaletteLight or PaletteDark
-
 		-- Override the internal padding constant to remove spaces
 		local constants = require("bufferline.constants")
 
 		constants.padding = ""
 
 		require("bufferline").setup({
-			highlights = {
-				fill = {
-					fg = is_light_mode and colors.noir_5 or colors.noir_4,
-					bg = colors.none,
-				},
-				duplicate = {
-					fg = is_light_mode and colors.noir_5 or colors.noir_4,
-					bg = colors.none,
-					italic = false,
-				},
-				duplicate_selected = {
-					fg = is_light_mode and colors.noir_0 or colors.text,
-					bg = colors.none,
-					italic = false,
-				},
-				duplicate_visible = {
-					fg = is_light_mode and colors.noir_4 or colors.noir_4,
-					bg = colors.none,
-					italic = false,
-				},
-				background = {
-					fg = is_light_mode and colors.noir_5 or colors.noir_4,
-					bg = colors.none,
-				},
-				close_button = {
-					fg = is_light_mode and colors.noir_5 or colors.noir_4,
-					bg = colors.none,
-				},
-				buffer_selected = {
-					fg = is_light_mode and colors.noir_0 or colors.text,
-					bg = is_light_mode and colors.none or colors.none,
-					bold = true,
-					italic = false,
-				},
-				close_button_selected = {
-					fg = is_light_mode and colors.noir_0 or colors.text,
-					bg = is_light_mode and colors.none or colors.none,
-				},
-				buffer_visible = {
-					fg = is_light_mode and colors.noir_3 or colors.noir_4,
-					bg = colors.none,
-					bold = false,
-					italic = false,
-				},
-				close_button_visible = {
-					fg = is_light_mode and colors.noir_4 or colors.noir_4,
-					bg = colors.none,
-				},
-				offset_separator = {
-					fg = colors.none,
-					bg = colors.none,
-				},
-				indicator_visible = {
-					fg = colors.none,
-					bg = colors.none,
-				},
-				indicator_selected = {
-					fg = is_light_mode and colors.noir_0 or colors.none,
-					bg = is_light_mode and colors.none or colors.none,
-				},
-				modified = {
-					fg = is_light_mode and colors.noir_4 or colors.noir_4,
-					bg = colors.none,
-				},
-				modified_visible = {
-					fg = is_light_mode and colors.noir_3 or colors.noir_4,
-					bg = colors.none,
-				},
-				modified_selected = {
-					fg = is_light_mode and colors.noir_0 or colors.text,
-					bg = is_light_mode and colors.none or colors.none,
-				},
-				separator = {
-					fg = is_light_mode and colors.noir_7 or colors.noir_4,
-					bg = colors.none,
-				},
-				separator_selected = {
-					fg = is_light_mode and colors.noir_7 or colors.none,
-					bg = is_light_mode and colors.noir_9 or colors.none,
-				},
-				separator_visible = {
-					fg = is_light_mode and colors.noir_7 or colors.noir_4,
-					bg = colors.none,
-				},
-			},
 			options = {
 				tab_size = 0,
 				max_name_length = 20,
@@ -129,7 +38,7 @@ return {
 				show_close_icon = true,
 				show_duplicate_prefix = true,
 				show_tab_indicators = false,
-				left_trunc_marker = "...",
+				left_trunc_marker = "... ",
 				right_trunc_marker = "...",
 				buffer_close_icon = "    ",
 				always_show_bufferline = true,
